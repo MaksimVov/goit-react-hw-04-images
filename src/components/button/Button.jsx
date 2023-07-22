eab7f2cfc5ae997776be62aa35f9028f96a62ea4
MaksimVov <vovchokmakcim@gmail.com>
2023-07-22 16:58:48 +0300

    re

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 export const Button = ({ onClick }) => {
   return (
     <button onClick={onClick} className={styles.Button}>
-      Load more.
+      Load more
     </button>
   );
 };
